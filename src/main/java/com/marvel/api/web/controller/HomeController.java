7314a6bc664f7e7f7e2e6905e272fe80380db5cd
nataliagrosa <64997042+nataliagrosa@users.noreply.github.com>
2020-11-11 22:47:06 -0300

    Update HomeController.java

--- src/main/java/com/marvel/api/web/controller/HomeController.java
+++ src/main/java/com/marvel/api/web/controller/HomeController.java
@@ -10,6 +10,6 @@ public class HomeController {
 
     @GetMapping
     public String home(){
-        return "Marvel API Homepage";
+        return "Marvel API Homepage by Azure Pipelines";
     }
 }
